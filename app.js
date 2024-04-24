7a50b92790b05a765c2bec9d6383d44386ce48ee
Игорь Щука <admin1c@lidkon.local>
2024-04-24 17:01:14 +0300

    change

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
 const express = require('express')
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:false,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 
 const app = express()
-const arr = [
-    {title:'ss'},
-    {title:'ss1'},
-    {title:'s2'},
-    {title:'ss3'},
+const freeTickets = [
+  {id:1, freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:2, freeTickets:7,isWeekend:false,weekOfDay:'Вт'},
+  {id:3, freeTickets:0,isWeekend:false,weekOfDay:'Ср'},
+  {id:4, freeTickets:12,isWeekend:false,weekOfDay:'Чт'},
+  {id:5, freeTickets:12,isWeekend:false,weekOfDay:'Пт'},
+  {id:6, freeTickets:5,isWeekend:false,weekOfDay:'Сб'},
+  {id:7, freeTickets:7,isWeekend:false,weekOfDay:'Вс'},
+  {id:8, freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:9, freeTickets:12,isWeekend:false,weekOfDay:'Вт'},
+  {id:10,freeTickets:0,isWeekend:false,weekOfDay:'Ср'},
+  {id:11,freeTickets:5,isWeekend:false,weekOfDay:'Чт'},
+  {id:12,freeTickets:12,isWeekend:false,weekOfDay:'Пт'},
+  {id:13,freeTickets:12,isWeekend:false,weekOfDay:'Сб'},
+  {id:14,freeTickets:12,isWeekend:true,weekOfDay:'Пн'},
+  {id:15,freeTickets:12,isWeekend:true,weekOfDay:'Пн'},
+  {id:16,freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:17,freeTickets:5,isWeekend:false,weekOfDay:'Пн'},
+  {id:18,freeTickets:7,isWeekend:false,weekOfDay:'Пн'},
+  {id:19,freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:20,freeTickets:0,isWeekend:false,weekOfDay:'Пн'},
+  {id:21,freeTickets:5,isWeekend:true,weekOfDay:'Пн'},
+  {id:22,freeTickets:7,isWeekend:true,weekOfDay:'Пн'},
+  {id:23,freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:24,freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:25,freeTickets:0,isWeekend:false,weekOfDay:'Пн'},
+  {id:26,freeTickets:12,isWeekend:false,weekOfDay:'Пн'},
+  {id:27,freeTickets:5,isWeekend:false,weekOfDay:'Пн'},
+  {id:28,freeTickets:0,isWeekend:true,weekOfDay:'Пн'},
+  {id:29,freeTickets:12,isWeekend:true,weekOfDay:'Пн'},
+ {title:30,freeTickets:1,isWeekend:false,weekOfDay:'Пн'},
+]
+
+const tickets = [
+  
 ]
 app.use(express.json());
+app.use(cors())
 app.get("/",(request,response)=>{
-   
-  response.send(arr)
+  const headers = {'Content-Type':'application/json',
+  'Access-Control-Allow-Origin':'*',
+  'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
+
+response.header = headers
+response.send(freeTickets)
+
 })
-app.listen(process.env.PORT)
+
+// app.listen(process.env.PORT)
+app.listen(3001)
