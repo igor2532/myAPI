d3f183ab3f6b22b9eb8120692aff10a3c17fcc8a
unknown <igorvitalevich1994@gmail.com>
2024-04-26 01:37:35 +0300

    final day

--- app.js
+++ app.js
@@ -43,25 +43,26 @@ app.use(cors())
 app.use(express.json());
 
 
-app.post("/insert", urlencodedParser,function(req, res){
-  pool.query("INSERT INTO `tickets` (`email`, `name`, `date`, `phone`, `countTickets`, `summ`, `active`, `dateOrder`) VALUES ('"+req.body.valueEmail+"', '"+req.body.valueName+"',  DATE_ADD('"+req.body.valueDateReservation+"', INTERVAL 3 HOUR), '"+req.body.valuePhone+"', '"+req.body.countTickets+"', '0', '1', DATE_ADD(NOW(), INTERVAL 3 HOUR));", function(err, results) {
+
+
+
+app.post("/deleteTicket", urlencodedParser,function(req, res){
+  pool.query("DELETE FROM tickets WHERE tickets.id = "+req.body.idTicket+";", function(err, results) {
     if(err) console.log(err);
      console.log(req.body.obj)
   });
 
-// app.post("/insert", urlencodedParser,function(req, res){
-//   pool.query("INSERT INTO `tickets` (`email`, `name`, `date`, `phone`, `countTickets`, `summ`, `active`, `dateOrder`) VALUES ('"+req.body.valueEmail+"', '"+req.body.valueName+"',  '"+req.body.valueDateReservation+"', '"+req.body.valuePhone+"', '"+req.body.countTickets+"', '0', '1', DATE_ADD(NOW(), INTERVAL 3 HOUR));", function(err, results) {
-//     if(err) console.log(err);
-//      console.log(req.body.obj)
-//   });
+})
 
 
 
-
-
+app.post("/insert", urlencodedParser,function(req, res){
+  pool.query("INSERT INTO `tickets` (`email`, `name`, `date`, `phone`, `countTickets`, `summ`, `active`, `dateOrder`) VALUES ('"+req.body.valueEmail+"', '"+req.body.valueName+"',  DATE_ADD('"+req.body.valueDateReservation+"', INTERVAL 3 HOUR), '"+req.body.valuePhone+"', '"+req.body.countTickets+"', '0', '1', DATE_ADD(NOW(), INTERVAL 3 HOUR));", function(err, results) {
+    if(err) console.log(err);
+     console.log(req.body.obj)
+    });
+  })
   
-});
-
 
 
 app.get("/dates/:date", function(req, res){
@@ -73,11 +74,31 @@ app.get("/dates/:date", function(req, res){
 
 
 app.get("/",(request,response)=>{
-pool.query("SELECT DATE_FORMAT(fixPlaces.date, '%d-%m-%Y') as dmw,DATE_FORMAT(fixPlaces.date, '%w') as week, DATE_FORMAT(fixPlaces.date, '%d') as day, DATE_FORMAT(fixPlaces.date, '%m-%Y') as dateMonth,date(fixPlaces.date) as dateValue,IFNULL((SELECT sum(fixPlaces.count) FROM fixPlaces WHERE DATE_FORMAT(fixPlaces.date, '%m-%Y') = '04-2024' AND DATE_FORMAT(fixPlaces.date, '%d-%m-%Y') = DATE_FORMAT(tickets.date, '%d-%m-%Y') ),SUM(fixPlaces.count))-SUM(IFNULL(IF(tickets.active,tickets.countTickets, IF( DATE_ADD(tickets.dateOrder, INTERVAL 1 DAY) > DATE_ADD(NOW(), INTERVAL 3 HOUR),tickets.countTickets,0) ),0)) as countsFreeTickets FROM fixPlaces LEFT JOIN tickets ON fixPlaces.date = tickets.date WHERE DATE_FORMAT(fixPlaces.date, '%m-%Y') = '04-2024' GROUP BY date(fixPlaces.date) order by fixPlaces.date ASC;", function(err, results) {
+pool.query("SELECT IF(NOW()>fixPlaces.date,'1','0') as isCloseReserv, DATE_FORMAT(fixPlaces.date, '%d-%m-%Y') as dmw,DATE_FORMAT(fixPlaces.date, '%w') as week, DATE_FORMAT(fixPlaces.date, '%d') as day, DATE_FORMAT(fixPlaces.date, '%m-%Y') as dateMonth,date(fixPlaces.date) as dateValue,IFNULL((SELECT sum(fixPlaces.count) FROM fixPlaces WHERE DATE_FORMAT(fixPlaces.date, '%m-%Y') = '04-2024' AND DATE_FORMAT(fixPlaces.date, '%d-%m-%Y') = DATE_FORMAT(tickets.date, '%d-%m-%Y') ),SUM(fixPlaces.count))-SUM(IFNULL(IF(tickets.active,tickets.countTickets, IF( DATE_ADD(tickets.dateOrder, INTERVAL 1 DAY) > DATE_ADD(NOW(), INTERVAL 3 HOUR),tickets.countTickets,0) ),0)) as countsFreeTickets FROM fixPlaces LEFT JOIN tickets ON fixPlaces.date = tickets.date WHERE DATE_FORMAT(fixPlaces.date, '%m-%Y') = '04-2024' GROUP BY date(fixPlaces.date) order by fixPlaces.date ASC;", function(err, results) {
   if(err) console.log(err);
   response.send(results)
 });
 })
 
-app.listen(process.env.PORT)
-// app.listen(3001)
+
+app.get("/tickets/",(request,response)=>{
+pool.query("SELECT * FROM `tickets` ORDER BY `dateOrder` DESC", function(err, results) {
+  if(err) console.log(err);
+  response.send(results)
+});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.listen(process.env.PORT)
+app.listen(3001)
