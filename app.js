5f3f1944c97bd69a67a18b2c908ebb45b6acccfa
unknown <igorvitalevich1994@gmail.com>
2024-04-27 13:35:21 +0300

    prod

--- app.js
+++ app.js
@@ -100,5 +100,5 @@ pool.query("SELECT * FROM `tickets` ORDER BY `dateOrder` DESC", function(err, re
 
 
 
-// app.listen(process.env.PORT)
-app.listen(3001)
+app.listen(process.env.PORT)
+// app.listen(3001)
